8a0930d048ed91ccc7352e009379075babba76af
hfk <k919893991@163.com>
2023-07-05 23:38:38 +0800

    csww

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello ceshi!");
-        System.out.println("Hello world!");
+        System.out.println("Hello world222!");
         System.out.println("Hello world!");
 
     }
